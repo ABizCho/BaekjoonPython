77e27c4266e3d7e43e3d09b22d4deab861a6b1c0
horiz.d <65459616+ABizCho@users.noreply.github.com>
2021-11-16 09:55:16 +0000

    solved math7_2839

--- math7_2839_sugar.py
+++ math7_2839_sugar.py
@@ -1,46 +1,12 @@
 n = int(input())
+cnt = 0
 
-A = 3
-B = 5
-
-
-# if n % B == 0 :
-  
-#   print(n//B)
-# else :
-#   if (n % B) % A == 0 :
-#     print((n//B)+((n%B)//A))
-    
-#   else :
-#     if n%A == 0 :
-#       print(n//A)
-#     else :
-#       print(-1)
-
-
-i = 0
-A1=  A
-abBreak = True
-while 1 :
-  B1 = B * i
-
-  
-  if B1 == n :
-    print(i)
+while n >= 0 :
+  if n % 5 == 0 :
+    cnt += n // 5
+    print(cnt)
     break
-  else :
-    for j in range(300):
-      A1 = A*j
-      if B1+A1 == n:
-        abBreak = False
-        
-        op1 = i+j+2
-        break
-
-    if op 1 < i+j+2 :
-  i += 1  
-
-
-
-
-
+  n -= 3
+  cnt += 1
+else :
+  print(-1)
